4232fe7beb63e46ef6b10234815a4bf15918e5f2
Hifza Khalid <hifzaofpk@gmail.com>
2025-03-05 16:55:09 +0500

    Create python_comments.py

---
+++ python_comments.py
@@ -0,0 +1,23 @@
+# Single-line comment: This program demonstrates comments in Python.
+
+"""
+Multi-line comment:
+This script includes examples of:
+1. Single-line comments
+2. Multi-line comments
+3. Docstrings in functions
+"""
+
+# Function with a docstring
+def example_function():
+    """This is a docstring explaining the function."""
+    print("Hello, Python!")
+
+# Calling the function
+example_function()
+
+'''
+Another multi-line comment:
+Useful for explaining complex logic.
+'''
+print("Python supports multiple ways to comment!")  # Inline comment
